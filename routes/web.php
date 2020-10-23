89ac4a3756ff26afe244ba35ce33b087415a0f6d
Khasan Erkinov <Khasan.7595@gmail.com>
2020-10-23 17:51:58 -0400

    mail is setup and working

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{theme?}','PublicController@index');
+Route::post('/{theme?}','PublicController@contactUs');
+
